a94ef161e09bad238074fdc8ad1f55f8e6f22136
fuji-Yuto <fujie.yuto.104@s.kyushu-u.ac.jp>
2023-09-12 20:16:42 +0900

    授業でやったとこ

--- app/Models/Tweet.php
+++ app/Models/Tweet.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Tweet extends Model
 {
     use HasFactory;
+    // アプリケーション側でcreateなどできない値を記述する
+    // 以下の処理を記述
+    protected $guarded = [
+        'id',
+        'created_at',
+        'updated_at',
+      ];
+    public static function getAllOrderByUpdated_at()
+    {
+        return self::orderBy('updated_at', 'desc')->get();
+    }
+    
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -31,6 +31,11 @@
                 </div>
 
                 <!-- Mypage links -->
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('tweet.mypage')" :active="request()->routeIs('tweet.mypage')">
+                        {{ __('Mypage') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
@@ -96,6 +101,13 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('tweet.create')" :active="request()->routeIs('tweet.create')">
                 {{ __('Create') }}
+            </x-responsive-nav-link>
+        </div>
+
+        <!-- 🔽 マイページへのリンクを追加 -->
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('tweet.mypage')" :active="request()->routeIs('tweet.mypage')">
+                {{ __('Mypage') }}
             </x-responsive-nav-link>
         </div>
 
